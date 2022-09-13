10a6d997324eaf9f6e6349796f79ccd19602a616
Mikita Nikanovich <mikita_nikanovich@epam.com>
2022-09-13 17:28:43 +0300

    chore: improve scripts

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,5 +1,15 @@
 echo "Installing yarn dependencies..."
 yarn install
 
-echo "Generating config files..."
-yarn rnuc configs/.env.dev
+echo "Generating config files.. ENVIRONMENT_VARIABLE $ENVIRONMENT_VARIABLE"
+if ["$ENVIRONMENT_VARIABLE" == 'stories'];
+  then yarn rnuc configs/.stories.dev
+fi
+
+if ["$ENVIRONMENT_VARIABLE" == 'qa'] 
+  then yarn rnuc configs/.dev.dev
+fi
+
+if ["$ENVIRONMENT_VARIABLE" == 'dev'] 
+  then yarn rnuc configs/.dev.dev
+fi
